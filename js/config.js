bf972159d97ee0efd02e48c83f4aac33d8fb5412
jft0046 <145061185+jft0046@users.noreply.github.com>
2024-04-05 00:04:53 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_BaDDNp0xT',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '4j7896bi0rggngo4af4ub8up8u',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_BaDDNp0xT/.well-known/jwks.json'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
