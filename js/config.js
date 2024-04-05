f54291a141c80d38573f505540bc47fcf37f6222
jft0046 <145061185+jft0046@users.noreply.github.com>
2024-04-05 00:55:27 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_BaDDNp0xT/.well-known/jwks.json'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://d7p6b8q3dc.execute-api.us-east-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
